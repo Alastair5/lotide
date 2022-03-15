87aa6c610068d3fbc5f25171fe7cd8419079fb0a
Alastair <97710359+Alastair5@users.noreply.github.com>
2022-03-14 17:47:40 -0700

    Update module export

--- tail.js
+++ tail.js
@@ -1,10 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
-  } else console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-};
-
-
 const tail = function(words) {
   const array = [];
   let k = 0;
@@ -15,6 +8,4 @@ const tail = function(words) {
   return array;
 };
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3);
+module.exports = tail;
