fbf938e6fc7e1cbd2fae1f24b3e6c9522ecc08dc
Alastair <97710359+Alastair5@users.noreply.github.com>
2022-03-14 18:20:33 -0700

    Update module export

--- middle.js
+++ middle.js
@@ -1,27 +1,3 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-    if (Array.isArray(array1) && Array.isArray(array2)) {
-      if (!eqArrays(array1[i], array2[i])) {
-        return false;
-      }
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(array1, array2) {
-  const final = eqArrays(array1, array2);
-  if (final) {
-    console.log(`🟢🟢🟢 Assertion Passed: ${array1} === ${array2}`);
-  } else console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
-};
-
 function middle(array) {
   let index = Math.floor(array.length / 2)
   let result = [];
@@ -40,4 +16,6 @@ function middle(array) {
   }
 };
 
+
+module.exports = middle;
 
